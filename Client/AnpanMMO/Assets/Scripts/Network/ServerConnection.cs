7a5d8cbd66b1066d18be842745c4b9020d6bdf68
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-03-31 09:50:45 +0900

    サーバ接続クラス

---
+++ Client/AnpanMMO/Assets/Scripts/Network/ServerConnection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+
+namespace Network
+{
+    /// <summary>
+    /// サーバとの接続
+    /// </summary>
+    public class ServerConnection
+    {
+        /// <summary>
+        /// TCPクライアント
+        /// </summary>
+        private TcpClient Client = null;
+
+        /// <summary>
+        /// 接続されているか？
+        /// </summary>
+        public bool IsConnected { get { return (Client != null && Client.Connected); } }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ServerConnection()
+        {
+        }
+
+        /// <summary>
+        /// 接続
+        /// TODO:ConnectAsyncを使った非同期処理に置き換える
+        /// </summary>
+        /// <param name="Host">ホスト</param>
+        /// <param name="Port">ポート</param>
+        /// <returns>接続したらtrue</returns>
+        public bool Connect(string Host, int Port)
+        {
+            try
+            {
+                Client = new TcpClient();
+                Client.Connect(Host, Port);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 切断
+        /// </summary>
+        public void Disconnect()
+        {
+            if (Client == null) { return; }
+            Client.Close();
+            Client = null;
+        }
+    }
+}
