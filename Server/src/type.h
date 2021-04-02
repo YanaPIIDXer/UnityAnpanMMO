36b898c3e9624b7906de84eac55ce3fd4f2fdcb2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-03 08:22:10 +0900

    C#（クライアント）と統一するための型定義を追加

---
+++ Server/src/type.h
@@ -0,0 +1,10 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+// C#と統一する為の型定義
+typedef unsigned char byte;
+typedef unsigned long ulong;
+#include <string>
+typedef std::string string; // using namespace stringよりはぶっちゃけマシ
+
+#endif // #ifndef TYPE_H
