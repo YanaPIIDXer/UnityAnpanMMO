a5e864c735b5f23f07da1de04ad9f94b44aafb9a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-12 18:11:52 +0900

    起動時にとりあえずエリアを生成するようにした

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "core/Application.h"
+#include "area/AreaManager.h"
 
 int main()
 {
     std::cout << "Start Unity AnpanMMO Server..." << std::endl;
+
+    // とりあえずエリア生成
+    AreaManager::GetInstance().Make(1);
 
     Application App;
     if (!App.StartListen(1234))
