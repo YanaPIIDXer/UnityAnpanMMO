4797e44e549c3a0fe96df039baea6b4454a930ec
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-12 16:23:44 +0900

    テスト用のログ出力

--- Server/src/peerstate/PeerStateActive.cpp
+++ Server/src/peerstate/PeerStateActive.cpp
@@ -1,9 +1,11 @@
 #include "PeerStateActive.h"
+#include <iostream>
 
 // コンストラクタ
 PeerStateActive::PeerStateActive(Peer *pInParent)
     : PeerStateBase(pInParent)
 {
+    std::cout << "PeerStateActive::PeerStateActive" << std::endl;
 }
 
 // デストラクタ
