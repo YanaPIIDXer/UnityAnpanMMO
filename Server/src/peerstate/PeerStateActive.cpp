31477a66023e4f1b934030fd0fbbd1c1c9524b24
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-12 18:44:06 +0900

    仮の入退場処理

--- Server/src/peerstate/PeerStateActive.cpp
+++ Server/src/peerstate/PeerStateActive.cpp
@@ -1,13 +1,19 @@
 #include "PeerStateActive.h"
 #include <iostream>
+#include "core/Peer.h"
+#include "area/AreaManager.h"
+#include "area/Area.h"
 
 // コンストラクタ
 PeerStateActive::PeerStateActive(Peer *pInParent)
     : PeerStateBase(pInParent)
 {
+    // TODO:本来ならローディングステートを挟む
+    AreaManager::GetInstance().Get(1).lock()->JoinPlayer(GetParent()->GetCharacter());
 }
 
 // デストラクタ
 PeerStateActive::~PeerStateActive()
 {
+    AreaManager::GetInstance().Get(1).lock()->LeavePlayer(GetParent()->GetId());
 }
