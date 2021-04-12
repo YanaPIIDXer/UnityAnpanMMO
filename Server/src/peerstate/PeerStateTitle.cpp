aa486886c8129c0961b484dda27d747746649aa4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-12 16:21:41 +0900

    ログインパケットの処理を実装

--- Server/src/peerstate/PeerStateTitle.cpp
+++ Server/src/peerstate/PeerStateTitle.cpp
@@ -1,12 +1,13 @@
 #include "PeerStateTitle.h"
 #include "PeerStateActive.h"
-#include "packet/PacketID.h"
+#include "packet/PacketLogInRequest.h"
+#include "Packet/PacketLogInResult.h"
 
 // コンストラクタ
 PeerStateTitle::PeerStateTitle(Peer *pInParent)
     : PeerStateBase(pInParent)
 {
-    AddPacketFunc(PacketID::LogIn, std::bind(&PeerStateTitle::OnRecvLogIn, this, std::placeholders::_1));
+    AddPacketFunc(PacketID::LogInRequest, std::bind(&PeerStateTitle::OnRecvLogIn, this, std::placeholders::_1));
 }
 
 // デストラクタ
@@ -17,5 +18,13 @@ PeerStateTitle::~PeerStateTitle()
 // ログインパケットを受信した
 void PeerStateTitle::OnRecvLogIn(IMemoryStream *pStream)
 {
-    // TODO:実装
+    PacketLogInRequest Request;
+    if (Request.Serialize(pStream))
+    {
+        return;
+    }
+
+    // 何もしていないのでとりあえず成功を返している
+    PacketLogInResult Result(PacketLogInResult::ResultCode::Success);
+    SendPacket(&Result);
 }
