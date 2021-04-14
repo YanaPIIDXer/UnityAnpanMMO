f686c483a83759c89715e1613a1975faf2fb8616
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-14 19:14:58 +0900

    クライアント側のシリアライズ用動的配列クラス ※オーバーロードの関係で、扱う型がISerializableを実装しているクラスと言う制約付き

---
+++ Client/AnpanMMO/Assets/Scripts/Util/FlexArray.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YanaPOnlineUtil;
+using YanaPOnlineUtil.Stream;
+
+/// <summary>
+/// シリアライズ用動的配列
+/// </summary>
+/// <typeparam name="T">扱う型</typeparam>
+public class FlexArray<T> : List<T>, ISerializable
+    where T : ISerializable
+{
+    /// <summary>
+    /// シリアライズ
+    /// </summary>
+    /// <param name="Stream">ストリーム</param>
+    /// <returns>成功したらtrueを返す</returns>
+    public bool Serialize(IMemoryStream Stream)
+    {
+        int Len = Count;
+        if (!Stream.Serialize(ref Len)) { return false; }
+
+        if (Len > Count)
+        {
+            T[] InsertArray = new T[Len - Count];
+            AddRange(InsertArray);
+        }
+
+        for (int i = 0; i < Len; i++)
+        {
+            this[i].Serialize(Stream);
+        }
+
+        return true;
+    }
+}
