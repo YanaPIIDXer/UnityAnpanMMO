d859493d3c3a470d836515400417e1c98dd48652
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-15 17:26:37 +0900

    Ping送信クラス

---
+++ Client/AnpanMMO/Assets/Scripts/Network/PingSender.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UniRx;
+using Network.Packet;
+
+namespace Network
+{
+    /// <summary>
+    /// Ping送受信
+    /// </summary>
+    public class PingSender : MonoBehaviour
+    {
+        /// <summary>
+        /// タイムアウトまでの秒数
+        /// </summary>
+        private static readonly float TimeoutSec = 10.0f;
+
+        /// <summary>
+        /// 最後にPingを受信してからの経過時間
+        /// </summary>
+        private float ElapsedTime = 0.0f;
+
+        /// <summary>
+        /// タイムアウトしたか？
+        /// </summary>
+        private bool bIsTimeout = false;
+
+        /// <summary>
+        /// Pingを受信しているか？
+        /// </summary>
+        private bool bRecvPing = false;
+
+        /// <summary>
+        /// タイムアウトSubject
+        /// </summary>
+        private Subject<Unit> TimeoutSubject = new Subject<Unit>();
+
+        /// <summary>
+        /// タイムアウトした
+        /// </summary>
+        public IObservable<Unit> OnTimeout { get { return TimeoutSubject; } }
+
+        void Awake()
+        {
+            GameServerConnection.Instance.OnRecvPacket
+                .Select((Data) => Data.Id == PacketID.Ping)
+                .Subscribe((_) => bRecvPing = true);
+        }
+
+        void Update()
+        {
+            if (bIsTimeout) { return; }
+            ElapsedTime += Time.deltaTime;
+            if (ElapsedTime >= TimeoutSec)
+            {
+                if (!bRecvPing)
+                {
+                    bIsTimeout = true;
+                    TimeoutSubject.OnNext(Unit.Default);
+                    return;
+                }
+                GameServerConnection.Instance.SendPacket(new PacketPing());
+            }
+        }
+    }
+}
