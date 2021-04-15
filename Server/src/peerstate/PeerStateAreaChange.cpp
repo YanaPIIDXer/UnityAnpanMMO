3f9c1a60de201c7104bd72204d12fa77f0dfc6d7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-15 23:01:39 +0900

    中身を実装

--- Server/src/peerstate/PeerStateAreaChange.cpp
+++ Server/src/peerstate/PeerStateAreaChange.cpp
@@ -1,12 +1,30 @@
 #include "PeerStateAreaChange.h"
+#include "core/Peer.h"
+#include "packet/PacketAreaChange.h"
+#include "packet/PacketPlayerPositionChange.h"
+#include "PeerStateActive.h"
 
 // コンストラクタ
-PeerStateAreaChange::PeerStateAreaChange(Peer *pInParent)
-    : PeerStateBase(pInParent)
+PeerStateAreaChange::PeerStateAreaChange(Peer *pInParent, uint InAreaId, const Vector &InPosition, float InRotation)
+    : PeerStateBase(pInParent), AreaId(InAreaId), Position(InPosition), Rotation(InRotation)
 {
+    AddPacketFunc(PacketID::AreaLoadEnd, std::bind(&PeerStateAreaChange::OnRecvLoadEnd, this, std::placeholders::_1));
+
+    PacketAreaChange Packet(AreaId);
+    pInParent->SendPacket(&Packet);
 }
 
 // デストラクタ
 PeerStateAreaChange::~PeerStateAreaChange()
 {
+}
+
+// ロード完了を受信した
+void PeerStateAreaChange::OnRecvLoadEnd(IMemoryStream *pStream)
+{
+    PositionPack PosPack(Position.X, Position.Y, Position.Z, Rotation);
+    PacketPlayerPositionChange Packet(PosPack);
+    SendPacket(&Packet);
+
+    SetNextState(new PeerStateActive(GetParent()));
 }
--- Server/src/peerstate/PeerStateAreaChange.h
+++ Server/src/peerstate/PeerStateAreaChange.h
@@ -2,19 +2,32 @@
 #define PEERSTATEAREACHANGE_H
 
 #include "PeerStateBase.h"
+#include "type.h"
+#include "math/Vector.h"
 
 // PeerState: エリア切り替え
 class PeerStateAreaChange : public PeerStateBase
 {
 public:
     // コンストラクタ
-    PeerStateAreaChange(Peer *pInParent);
+    PeerStateAreaChange(Peer *pInParent, uint InAreaId, const Vector &InPosition, float InRotation);
 
     // デストラクタ
     virtual ~PeerStateAreaChange();
 
 protected:
 private:
+    // エリアＩＤ
+    uint AreaId;
+
+    // 座標
+    Vector Position;
+
+    // 回転
+    float Rotation;
+
+    // ロード完了を受信した
+    void OnRecvLoadEnd(IMemoryStream *pStream);
 };
 
 #endif // #ifndef PEERSTATEAREACHANGE_H
