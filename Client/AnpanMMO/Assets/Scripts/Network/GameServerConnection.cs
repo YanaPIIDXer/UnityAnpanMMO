f377c4ae693be56780879c9cb5a8449f6b5df258
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-15 17:15:05 +0900

    パケット送受信処理

--- Client/AnpanMMO/Assets/Scripts/Network/GameServerConnection.cs
+++ Client/AnpanMMO/Assets/Scripts/Network/GameServerConnection.cs
@@ -1,9 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using System;
+using Network.Packet;
+using YanaPOnlineUtil.Stream;
+using YanaPOnlineUtil.Packet;
 
 namespace Network
 {
+    /// <summary>
+    /// 受信データ
+    /// </summary>
+    public class ReceiveData
+    {
+        /// <summary>
+        /// パケットＩＤ
+        /// </summary>
+        public PacketID Id { get; private set; }
+
+        /// <summary>
+        /// ストリーム
+        /// </summary>
+        public IMemoryStream Stream { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="Id">パケットＩＤ</param>
+        /// <param name="Stream">ストリーム</param>
+        public ReceiveData(PacketID Id, IMemoryStream Stream)
+        {
+            this.Id = Id;
+            this.Stream = Stream;
+        }
+    }
+
     /// <summary>
     /// ゲームサーバとの接続管理
     /// </summary>
@@ -19,9 +51,29 @@ namespace Network
         /// </summary>
         private ServerConnection Connection = new ServerConnection();
 
+        /// <summary>
+        /// パケットシリアライザ
+        /// </summary>
+        private PacketSerializer Serializer = null;
+
+        /// <summary>
+        /// パケット受信Subject
+        /// </summary>
+        private Subject<ReceiveData> PacketSubject = new Subject<ReceiveData>();
+
+        /// <summary>
+        /// パケット受信Observable
+        /// </summary>
+        public IObservable<ReceiveData> OnRecvPacket { get { return PacketSubject; } }
+
         void Awake()
         {
             Instance = this;
+            Serializer = new PacketSerializer((Id, Stream) =>
+            {
+                ReceiveData Data = new ReceiveData((PacketID)Id, Stream);
+                PacketSubject.OnNext(Data);
+            }, Connection.Send);
         }
 
         /// <summary>
@@ -36,12 +88,22 @@ namespace Network
         }
 
         /// <summary>
+        /// パケット送信
+        /// </summary>
+        /// <param name="Packet">パケット</param>
+        public void SendPacket(YanaPOnlineUtil.Packet.Packet Packet)
+        {
+            Serializer.Send(Packet);
+        }
+
+        /// <summary>
         /// データを受信した
         /// </summary>
         /// <param name="Data">データ</param>
         /// <param name="Length">データ長</param>
         private void OnRecvData(byte[] Data, int Length)
         {
+            Serializer.OnRecv(Data);
         }
     }
 }
