a5428b870547affd56d08b707db52f96ca4dfd61
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-15 21:30:43 +0900

    ログインボタン

---
+++ Client/AnpanMMO/Assets/Scripts/UI/LogInButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Network;
+
+namespace UI
+{
+    /// <summary>
+    /// ログインボタン
+    /// </summary>
+    public class LogInButton : MonoBehaviour
+    {
+        public void OnPressed()
+        {
+            if (!GameServerConnection.Instance.Connect())
+            {
+                Debug.Log("LogIn Failed...");
+                return;
+            }
+        }
+    }
+}
