f6b92ecc3dd6342656e5852908c97d965636b841
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-15 16:59:40 +0900

    ゲームサーバとの接続クラス

---
+++ Client/AnpanMMO/Assets/Scripts/Network/GameServerConnection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Network
+{
+    /// <summary>
+    /// ゲームサーバとの接続管理
+    /// </summary>
+    public class GameServerConnection : MonoBehaviour
+    {
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public static GameServerConnection Instance { get; private set; }
+
+        /// <summary>
+        /// サーバとの通信オブジェクト
+        /// </summary>
+        private ServerConnection Connection = new ServerConnection();
+
+        void Awake()
+        {
+            Instance = this;
+        }
+
+        /// <summary>
+        /// 接続
+        /// </summary>
+        /// <returns>成功したらtrueを返す</returns>
+        public bool Connect()
+        {
+            return Connection.Connect("127.0.0.1", 1234);
+        }
+    }
+}
