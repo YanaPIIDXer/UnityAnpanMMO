4568611f211fdd57316aebb055f10ff5967a28db
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-16 11:16:16 +0900

    エリア切り替えクラス

---
+++ Client/AnpanMMO/Assets/Scripts/Network/AreaChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Network.Packet;
+using System;
+using UniRx;
+
+namespace Network
+{
+    /// <summary>
+    /// エリア切り替え
+    /// </summary>
+    public class AreaChange : MonoBehaviour
+    {
+        /// <summary>
+        /// シーンファイルプレフィクス
+        /// </summary>
+        private static readonly string ScenePrefix = "Area";
+
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+            GameServerConnection.OnRecvPacket
+                .Where((Data) => Data.Id == PacketID.AreaChange)
+                .Subscribe((Data) =>
+                {
+                    Debug.Log("Area Change");
+                    // TODO:実際のロード処理
+                    GameServerConnection.Instance.SendPacket(new PacketAreaLoadEnd());
+                });
+        }
+    }
+}
