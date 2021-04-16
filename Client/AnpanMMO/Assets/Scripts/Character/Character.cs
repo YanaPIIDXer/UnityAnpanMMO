92a39957408724d52bc5860906aedd6f7c5be512
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-16 15:48:52 +0900

    キャラクタ基底クラスとエネミークラスをとりあえず定義 定義したエネミーComponentをPrefabにAttach

---
+++ Client/AnpanMMO/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character
+{
+    /// <summary>
+    /// キャラクタ基底クラス
+    /// </summary>
+    public abstract class Character : MonoBehaviour
+    {
+    }
+}
