3388f6e5644459611fd32cdc1f6375952cae7fcf
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-16 09:40:28 +0900

    ログイン結果受信クラス

---
+++ Client/AnpanMMO/Assets/Scripts/Network/LogInResultReceiver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UniRx;
+using Network.Packet;
+
+namespace Network
+{
+    /// <summary>
+    /// ログイン結果受信
+    /// </summary>
+    public class LogInResultReceiver : MonoBehaviour
+    {
+        /// <summary>
+        /// ログイン成功Subject
+        /// </summary>
+        private Subject<Unit> LogInSuccessSubject = new Subject<Unit>();
+
+        /// <summary>
+        /// ログインに成功した
+        /// </summary>
+        public IObservable<Unit> OnLogInSuccess { get { return LogInSuccessSubject; } }
+
+        void Awake()
+        {
+            GameServerConnection.OnRecvPacket
+                .Where((Data) => Data.Id == PacketID.LogInResult)
+                .Subscribe((Data) =>
+                {
+                    PacketLogInResult Result = new PacketLogInResult();
+                    Result.Serialize(Data.Stream);
+                    if (Result.Result != (byte)PacketLogInResult.ResultCode.Success)
+                    {
+                        Debug.Log("LogIn Failed...");
+                        return;
+                    }
+                    LogInSuccessSubject.OnNext(Unit.Default);
+                });
+        }
+    }
+}
