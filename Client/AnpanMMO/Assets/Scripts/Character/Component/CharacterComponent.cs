b5226a81221a0498a0597be45024a7badf0a9d36
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-16 15:53:08 +0900

    キャラクタコンポーネント基底クラス

---
+++ Client/AnpanMMO/Assets/Scripts/Character/Component/CharacterComponent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character.Component
+{
+    /// <summary>
+    /// キャラクタコンポーネント基底クラス
+    /// </summary>
+    public abstract class CharacterComponent
+    {
+        /// <summary>
+        /// 所有者
+        /// </summary>
+        private Character Owner = null;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="Owner">所有者</param>
+        public CharacterComponent(Character Owner)
+        {
+            this.Owner = Owner;
+        }
+
+        /// <summary>
+        /// MonoBehaviour派生のComponentを取得
+        /// ※GetComponent<T>()
+        /// </summary>
+        /// <typeparam name="T">Componentの型</typeparam>
+        /// <returns>Component</returns>
+        public T GetOwnerComponent<T>()
+        {
+            return Owner.GetComponent<T>();
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public virtual void OnInitialize() { }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        public virtual void OnUpdate() { }
+
+        /// <summary>
+        /// FixedUpdate
+        /// </summary>
+        public virtual void OnFixedUpdate() { }
+    }
+}
