fdc9b281bf7e199f6adf2b9b4f410a1b92973f07
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-20 18:37:04 +0900

    プレイヤーマネージャクラスをとりあえず定義

---
+++ Client/AnpanMMO/Assets/Scripts/Character/Player/PlayerManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character.Player
+{
+    /// <summary>
+    /// プレイヤーマネージャ
+    /// </summary>
+    public class PlayerManager : MonoBehaviour
+    {
+    }
+}
