82ca4775b22e0a70b65ba279280c266a3c4a2573
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-20 18:22:00 +0900

    プレイヤークラスとPrefab

---
+++ Client/AnpanMMO/Assets/Scripts/Character/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character.Player
+{
+    /// <summary>
+    /// プレイヤー
+    /// </summary>
+    public class Player : Character
+    {
+    }
+}
