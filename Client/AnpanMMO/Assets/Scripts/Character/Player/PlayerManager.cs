71cdd0a86d65f4df483472632298d7e488883c8e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-20 19:15:48 +0900

    プレイヤー生成処理

--- Client/AnpanMMO/Assets/Scripts/Character/Player/PlayerManager.cs
+++ Client/AnpanMMO/Assets/Scripts/Character/Player/PlayerManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Network;
+using Network.Packet;
 
 namespace Character.Player
 {
@@ -9,5 +11,30 @@ namespace Character.Player
     /// </summary>
     public class PlayerManager : MonoBehaviour
     {
+        /// <summary>
+        /// プレイヤーのPrefab
+        /// </summary>
+        private static GameObject PlayerPrefab = null;
+
+        private static readonly string PlayerPrefabPath = "Prefabs/Player";
+
+        void Awake()
+        {
+            GameServerConnection.PacketMethods[PacketID.PlayerPositionChange] = (Stream) =>
+            {
+                PacketPlayerPositionChange Packet = new PacketPlayerPositionChange();
+                Packet.Serialize(Stream);
+                // HACK:「キャラがまた生成されていない」と言う前提
+                if (PlayerPrefab == null)
+                {
+                    PlayerPrefab = Resources.Load<GameObject>(PlayerPrefabPath);
+                    Debug.Assert(PlayerPrefab != null);
+                }
+
+                Vector3 Position = new Vector3(Packet.Position.X, Packet.Position.Y, Packet.Position.Z);
+                float Rotation = Packet.Position.Rotation;
+                Instantiate<GameObject>(PlayerPrefab, Position, Quaternion.Euler(0.0f, Rotation, 0.0f));
+            };
+        }
     }
 }
