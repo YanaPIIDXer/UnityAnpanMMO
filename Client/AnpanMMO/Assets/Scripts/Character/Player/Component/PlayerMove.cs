ddc86bc6484d81a9b3d9ab4cb19f974d289bd135
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-20 18:33:11 +0900

    移動処理

--- Client/AnpanMMO/Assets/Scripts/Character/Player/Component/PlayerMove.cs
+++ Client/AnpanMMO/Assets/Scripts/Character/Player/Component/PlayerMove.cs
@@ -11,10 +11,35 @@ namespace Character.Player.Component
     public class PlayerMove : CharacterComponent
     {
         /// <summary>
+        /// Rididbody
+        /// </summary>
+        private Rigidbody RigidBody = null;
+
+        /// <summary>
         /// コンストラクタ
         /// </summary>
         public PlayerMove()
         {
+        }
+
+        /// <summary>
+        /// 初期化された
+        /// </summary>
+        public override void OnInitialize()
+        {
+            RigidBody = GetOwnerComponent<Rigidbody>();
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        public override void OnUpdate()
+        {
+            Vector3 MoveVec = Vector3.zero;
+            MoveVec.x = Input.GetAxis("Horizontal");
+            MoveVec.y = Input.GetAxis("Vertical");
+            MoveVec.Normalize();
+            RigidBody.velocity = MoveVec;
         }
     }
 }
