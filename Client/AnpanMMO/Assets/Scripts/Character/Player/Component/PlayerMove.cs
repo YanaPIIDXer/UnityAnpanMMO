efcd374bdef488de99513772eef5878247492606
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-20 18:25:04 +0900

    移動用Componentをとりあえず定義

---
+++ Client/AnpanMMO/Assets/Scripts/Character/Player/Component/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Character.Component;
+
+namespace Character.Player.Component
+{
+    /// <summary>
+    /// プレイヤー移動Component
+    /// </summary>
+    public class PlayerMove : CharacterComponent
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public PlayerMove()
+        {
+        }
+    }
+}
--- Client/AnpanMMO/Assets/Scripts/Character/Player/Player.cs
+++ Client/AnpanMMO/Assets/Scripts/Character/Player/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Character.Component;
+using Character.Player.Component;
 
 namespace Character.Player
 {
@@ -9,5 +11,9 @@ namespace Character.Player
     /// </summary>
     public class Player : Character
     {
+        void Awake()
+        {
+            AddCharacterComponent(new PlayerMove());
+        }
     }
 }
