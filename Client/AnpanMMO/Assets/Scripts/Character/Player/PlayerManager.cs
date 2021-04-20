69f5cabd284ee052f85061926e25ea5f2cb61fc9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-21 05:27:09 +0900

    生成系パケットのハンドリング

--- Client/AnpanMMO/Assets/Scripts/Character/Player/PlayerManager.cs
+++ Client/AnpanMMO/Assets/Scripts/Character/Player/PlayerManager.cs
@@ -16,7 +16,15 @@ namespace Character.Player
         /// </summary>
         private static GameObject PlayerPrefab = null;
 
+        /// <summary>
+        /// Prefabのパス
+        /// </summary>
         private static readonly string PlayerPrefabPath = "Prefabs/Player";
+
+        /// <summary>
+        /// プレイヤーDictionary
+        /// </summary>
+        private Dictionary<uint, Player> PlayerDic = new Dictionary<uint, Player>();
 
         void Awake()
         {
@@ -24,7 +32,6 @@ namespace Character.Player
             {
                 PacketPlayerPositionChange Packet = new PacketPlayerPositionChange();
                 Packet.Serialize(Stream);
-                // HACK:「キャラがまた生成されていない」と言う前提
                 if (PlayerPrefab == null)
                 {
                     PlayerPrefab = Resources.Load<GameObject>(PlayerPrefabPath);
@@ -33,8 +40,44 @@ namespace Character.Player
 
                 Vector3 Position = new Vector3(Packet.Position.X, Packet.Position.Y, Packet.Position.Z);
                 float Rotation = Packet.Position.Rotation;
-                Instantiate<GameObject>(PlayerPrefab, Position, Quaternion.Euler(0.0f, Rotation, 0.0f));
+                var PlayerObj = Instantiate<GameObject>(PlayerPrefab, Position, Quaternion.Euler(0.0f, Rotation, 0.0f));
+                PlayerDic.Add(PlayerData.PlayerId, PlayerObj.GetComponent<Player>());
             };
+
+            GameServerConnection.PacketMethods[PacketID.PlayerList] = (Stream) =>
+            {
+                PacketPlayerList Packet = new PacketPlayerList();
+                Packet.Serialize(Stream);
+                foreach (var Data in Packet.List)
+                {
+                    Spawn(Data);
+                }
+            };
+
+            GameServerConnection.PacketMethods[PacketID.PlayerEntry] = (Stream) =>
+            {
+                PacketPlayerEntry Packet = new PacketPlayerEntry();
+                Packet.Serialize(Stream);
+                Spawn(Packet.Data);
+            }
+        }
+
+        /// <summary>
+        /// Spawn
+        /// </summary>
+        /// <param name="Data">データ</param>
+        private void Spawn(Network.Packet.PlayerData Data)
+        {
+            if (PlayerPrefab == null)
+            {
+                PlayerPrefab = Resources.Load<GameObject>(PlayerPrefabPath);
+                Debug.Assert(PlayerPrefab != null);
+            }
+
+            Vector3 Position = new Vector3(Data.Position.X, Data.Position.Y, Data.Position.Z);
+            float Rotation = Data.Position.Rotation;
+            var PlayerObj = Instantiate<GameObject>(PlayerPrefab, Position, Quaternion.Euler(0.0f, Rotation, 0.0f));
+            PlayerDic.Add(Data.Id, PlayerObj.GetComponent<Player>());
         }
     }
 }
