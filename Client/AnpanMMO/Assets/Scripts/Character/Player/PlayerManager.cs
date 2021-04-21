4d61f0a30f67cefc23fa524844ae2b7567a2e0a8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-04-21 16:38:12 +0900

    長くなったのでメソッドに切り出した

--- Client/AnpanMMO/Assets/Scripts/Character/Player/PlayerManager.cs
+++ Client/AnpanMMO/Assets/Scripts/Character/Player/PlayerManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Network;
 using Network.Packet;
+using YanaPOnlineUtil.Stream;
 
 namespace Character.Player
 {
@@ -28,38 +29,65 @@ namespace Character.Player
 
         void Awake()
         {
-            GameServerConnection.PacketMethods[PacketID.PlayerPositionChange] = (Stream) =>
-            {
-                PacketPlayerPositionChange Packet = new PacketPlayerPositionChange();
-                Packet.Serialize(Stream);
-                if (PlayerPrefab == null)
-                {
-                    PlayerPrefab = Resources.Load<GameObject>(PlayerPrefabPath);
-                    Debug.Assert(PlayerPrefab != null);
-                }
+            GameServerConnection.PacketMethods[PacketID.PlayerPositionChange] = OnRecvPlayerPositionChange;
+            GameServerConnection.PacketMethods[PacketID.PlayerList] = OnRecvPlayerList;
+            GameServerConnection.PacketMethods[PacketID.PlayerEntry] = OnRecvPlayerEntry;
+            GameServerConnection.PacketMethods[PacketID.PlayerMoved] = OnRecvPlayerMoved;
+        }
 
-                Vector3 Position = new Vector3(Packet.Position.X, Packet.Position.Y, Packet.Position.Z);
-                float Rotation = Packet.Position.Rotation;
-                var PlayerObj = Instantiate<GameObject>(PlayerPrefab, Position, Quaternion.Euler(0.0f, Rotation, 0.0f));
-                PlayerDic.Add(PlayerData.PlayerId, PlayerObj.GetComponent<Player>());
-            };
-
-            GameServerConnection.PacketMethods[PacketID.PlayerList] = (Stream) =>
+        /// <summary>
+        /// プレイヤーの座標強制変更を受信した
+        /// </summary>
+        /// <param name="Stream">ストリーム</param>
+        private void OnRecvPlayerPositionChange(IMemoryStream Stream)
+        {
+            PacketPlayerPositionChange Packet = new PacketPlayerPositionChange();
+            Packet.Serialize(Stream);
+            if (PlayerPrefab == null)
             {
-                PacketPlayerList Packet = new PacketPlayerList();
-                Packet.Serialize(Stream);
-                foreach (var Data in Packet.List)
-                {
-                    Spawn(Data);
-                }
-            };
+                PlayerPrefab = Resources.Load<GameObject>(PlayerPrefabPath);
+                Debug.Assert(PlayerPrefab != null);
+            }
 
-            GameServerConnection.PacketMethods[PacketID.PlayerEntry] = (Stream) =>
+            Vector3 Position = new Vector3(Packet.Position.X, Packet.Position.Y, Packet.Position.Z);
+            float Rotation = Packet.Position.Rotation;
+            var PlayerObj = Instantiate<GameObject>(PlayerPrefab, Position, Quaternion.Euler(0.0f, Rotation, 0.0f));
+            PlayerDic.Add(PlayerData.PlayerId, PlayerObj.GetComponent<Player>());
+        }
+
+        /// <summary>
+        /// プレイヤーリストを受信した
+        /// </summary>
+        /// <param name="Stream">ストリーム</param>
+        private void OnRecvPlayerList(IMemoryStream Stream)
+        {
+            PacketPlayerList Packet = new PacketPlayerList();
+            Packet.Serialize(Stream);
+            foreach (var Data in Packet.List)
             {
-                PacketPlayerEntry Packet = new PacketPlayerEntry();
-                Packet.Serialize(Stream);
-                Spawn(Packet.Data);
-            };
+                Spawn(Data);
+            }
+        }
+
+        /// <summary>
+        /// プレイヤー参加を受信した
+        /// </summary>
+        /// <param name="Stream">ストリーム</param>
+        private void OnRecvPlayerEntry(IMemoryStream Stream)
+        {
+            PacketPlayerEntry Packet = new PacketPlayerEntry();
+            Packet.Serialize(Stream);
+            Spawn(Packet.Data);
+        }
+
+        /// <summary>
+        /// 他プレイヤーの移動を受信した
+        /// </summary>
+        /// <param name="Stream">ストリーム</param>
+        private void OnRecvPlayerMoved(IMemoryStream Stream)
+        {
+            PacketPlayerMoved Packet = new PacketPlayerMoved();
+            Packet.Serialize(Stream);
         }
 
         /// <summary>
